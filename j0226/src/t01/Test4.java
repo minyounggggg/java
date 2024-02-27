fb6d8e9f9118b360544df56cdff0fa0673597588
minyoung <minmin0325@naver.com>
2024-02-27 11:01:24 +0900

    first Project

---
+++ j0226/src/t01/Test4.java
@@ -0,0 +1,10 @@
+package t01;
+
+public class Test4 {
+	public static void main(String[] args) { //메인메소드, 실행 클래스, []배열
+		System.out.println("h\ti-01"); // \n 탭사이즈만큼 띄우기
+		System.out.print("hi-02\n\n"); //print뒤의 ln은 줄바꿈, \n도 줄바꿈
+		System.out.println("hi-03");
+		System.out.println("hi-04");
+	}
+}
